3d0a6f03d22b19c0eb0e876118820153350df783
frederic-icam2028 <frederic.viravanh@2028.icam.fr>
2026-03-31 21:40:10 +0200

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -4,7 +4,7 @@
 int main() {
 double pi = 3.1416;
 double rayon = 6;  
-    printf("L'air du cercle = %f", pi*(rayon*rayon));
+    printf("L'air du cercle = %f\n", pi*(rayon*rayon));
    printf("Le périmètre du cercle = %f", pi*(rayon+rayon));
 }
 
